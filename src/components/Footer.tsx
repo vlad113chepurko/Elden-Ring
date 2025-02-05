65380b8107b5e8c6303b705e3096ed228234b74e
Vlad Chepurko <vlad113chepurko@gmail.com>
2025-02-05 23:52:53 +0200

    Added <Footer.tsx /> and styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,27 @@
+import gitHub from "../assets/InputContainer/GitHub.svg";
+import linkedIn from "../assets/InputContainer/LinkedInCircled.svg";
+
 export default function Footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <section>
+        <h3 className="margin-bottom">Follow Me</h3>
+        <ul>
+          <li className="wrapper-li">
+            <a href="https://github.com/vlad113chepurko">
+              <img src={gitHub} alt="GitHub" />
+            </a>
+            <a href="https://github.com/vlad113chepurko">GitHub</a>
+          </li>
+          <li className="wrapper-li">
+            <a href="www.linkedin.com/in/vlad-chepurko-0953b32b0">
+              <img src={linkedIn} alt="linkedIn" />
+              <a href="https://github.com/vlad113chepurko">LinkedIn</a>
+            </a>
+          </li>
+        </ul>
+      </section>
+      <p>© 2025 Elden Ring. All rights reserved.</p>
+    </footer>
+  );
 }
